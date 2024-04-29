a879308948c9c5c54fd3e6dd10490a0f3475394b
alex.cherkaski <alexcher81@gmail.com>
2024-04-28 23:53:42 -0400

    Add function to move the fps counter to the bottom left corner.

--- Source/Systems/UIRenderSystem.cpp
+++ Source/Systems/UIRenderSystem.cpp
@@ -7,6 +7,27 @@
 #include "UIManager/UIManager.h"
 #include "UIRenderSystem.h"
 
+static bool MoveToBottomLeft(Entity avgFPSEntity)
+{
+	// Retrieve the window dimensions to translate the ui entity.
+	const Window& window = Window::GetInstanceRead();
+	const float xTranslation = window.GetClientWidth() / 2.0f;
+	const float yTranslation = window.GetClientHeight() / 2.0f - 20;
+
+	// Retrieve the ui entity transform for updating.
+	Registry& registry = Registry::GetInstanceWrite();
+	TransformComponent& transformComponent = registry.GetComponentWrite<TransformComponent>(avgFPSEntity);
+	
+	// Apply the translation to the entity transform.
+	const XMMATRIX translation = XMMatrixTranslation(-xTranslation, -yTranslation, 0.0f);
+	XMMATRIX transform = XMLoadFloat4x4(&transformComponent.Transform);
+	transform *= translation;
+	XMStoreFloat4x4(&transformComponent.Transform, transform);
+
+	// Dummy return value.
+	return true;
+}
+
 UIRenderSystem::UIRenderSystem(Registry& registry)
 	: ISystem::ISystem(registry)
 {
@@ -21,6 +42,9 @@ void UIRenderSystem::Update(float deltaTime)
 	static UIManager& uiManager = UIManager::GetInstanceWrite();
 	static Entity avgFPSEntity = *m_entities.cbegin(); // For testing purposes assume its the first.
 	static const UIComponent& uiComponent = m_registry.GetComponentRead<UIComponent>(avgFPSEntity);
+
+	// Move the ui element to the bottom left of the window.
+	static bool runOnce = MoveToBottomLeft(avgFPSEntity);
 
 	// Recreate the ui element mesh based on the new average fps value.
 	const std::wstring averageFPS = L"Average FPS: " + std::to_wstring(engine.GetAverageFPS());
